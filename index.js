0bf5581c3cf247ca210c59562c88f8a418688557
Hudson Souza <souzaehudson@gmail.com>
2025-08-09 15:06:50 -0300

    Third commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const menuButton = document.getElementById("menu");
+
+const linksListMobile = document.getElementById("mobileMenu");
+
+const bodyHtml = document.getElementById("body");
+const mainHtml = document.getElementById("main");
+
+function showLinksMobile() {
+  linksListMobile.style.display = "flex";
+}
+
+function closeLinksMobile() {
+  linksListMobile.style.display = "none";
+}
+
+menuButton.addEventListener("click", showLinksMobile);
+main.addEventListener("click", closeLinksMobile);
