1f7555fa959d2aa11e5e4141fd75502bea6d559d
Hudson Souza <souzaehudson@gmail.com>
2025-08-11 19:18:37 -0300

    Js commit

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const linksListMobile = document.getElementById("mobileMenu");
 const bodyHtml = document.getElementById("body");
 const mainHtml = document.getElementById("main");
 
+const relAno = document.getElementById("relAno");
+const noDie = document.getElementById("noDie");
+const loved = document.getElementById("loved");
+
+let index = 1;
+
+const album = [
+  "https://i.imgur.com/GmoJTdZ.jpeg",
+  "https://i.imgur.com/oyRASx0.jpeg",
+  "https://i.imgur.com/GmoJTdZ.jpeg",
+  "https://i.imgur.com/gmuEI0n.jpeg"
+]
+
 function showLinksMobile() {
   linksListMobile.style.display = "flex";
 }
@@ -12,6 +25,14 @@ function showLinksMobile() {
 function closeLinksMobile() {
   linksListMobile.style.display = "none";
 }
+
+function slideShow () {
+  relAno.src = album.[index];
+  noDie.src = album.[index];
+  loved.src = album.[index];
+}
+
+slideShow();
 
 menuButton.addEventListener("click", showLinksMobile);
 main.addEventListener("click", closeLinksMobile);
